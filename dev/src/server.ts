c4964b929629c39be375dbd0673ab7a919cd1d93
will kent-daggett <3254957+wkentdag@users.noreply.github.com>
2024-04-16 18:01:28 -0700

    chore: add test:all runner for local dev

--- dev/src/server.ts
+++ dev/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
-import payload from 'payload'
-import { InitOptions } from 'payload/config'
+import payload, { type Payload } from 'payload'
+import { type InitOptions } from 'payload/config'
 
+const PORT = process.env.PORT || 3000
+
+// eslint-disable-next-line
 require('dotenv').config()
 const app = express()
 
@@ -10,25 +13,26 @@ app.get('/', (_, res) => {
   res.redirect('/admin')
 })
 
-export const start = async (args?: Partial<InitOptions>) => {
+export const start = async (args?: Partial<InitOptions>): Promise<void|Payload> => {
   // Initialize Payload
   const client = await payload.init({
     secret: process.env.PAYLOAD_SECRET,
     express: app,
-    onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
-    },
+    // onInit: async _payload => {
+    //   _payload.logger.info(`Payload Admin URL: ${_payload.getAdminURL()}`)
+    // },
     ...(args || {}),
   })
 
-  if (!args?.local === true) {
-    app.listen(3000)
-    payload.logger.info(`Payload listening on port ${3000}`)
-  } else {
-    // nb in jest, this is added to the global namespace
-    // https://payloadcms.com/docs/local-api/overview#nextjs-conflict-with-local-api
-    return client
+  if (!(args?.local === true)) {
+    app.listen(PORT)
+    payload.logger.info(`Payload listening on port ${PORT}`)
+    return null
   }
+
+  // nb in jest, this is added to the global namespace
+  // https://payloadcms.com/docs/local-api/overview#nextjs-conflict-with-local-api
+  return client
 }
 
 start()
